a643af426eef2754e780e186605cd436f687da2a
MicrosftAzureAaron <aarosanders@microsoft.com>
2025-05-12 21:25:12 -0500

    edit bootscript

--- firstboot.sh
+++ firstboot.sh
@@ -15,6 +15,6 @@ sudo apt-get install -y wireguard
 # Enable and start the WireGuard service
 echo "Enabling and starting WireGuard service..."
 sudo systemctl enable wg-quick@wg0
-sudo systemctl start wg-quick@wg0
+#sudo systemctl start wg-quick@wg0
 
 echo "WireGuard installation and setup complete."
